128dab8c38bfc1b3f8b271d6b91c2eb25557c077
R1sh1007 <rishiporwalbid@gmail.com>
2026-03-12 11:26:17 +0530

    update

--- app/src/main/java/com/deendayalproject/fragments/composeui/main/InspectionSectionWiseUi.kt
+++ app/src/main/java/com/deendayalproject/fragments/composeui/main/InspectionSectionWiseUi.kt
@@ -437,7 +437,6 @@ fun InspectionStepModernScreen(
                                             )
 
                                     }
-
                                 }
 
                             }
