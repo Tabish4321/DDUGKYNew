4fa2ef279a058aa33ca879274082ae2869281778
Tabish <tjamal333@gmail.com>
2026-06-03 12:11:23 +0530

    trasanction change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.deendayalproject"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.0.3"
+        versionCode = 20
+        versionName = "2.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
